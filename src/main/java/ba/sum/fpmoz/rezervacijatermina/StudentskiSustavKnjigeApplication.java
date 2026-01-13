6f61aa8b469ec0568320e7bc4718111c8d069d80
Sara <sara.budimir@fpmoz.sum.ba>
2026-01-13 17:19:09 +0100

    Spring Boot, Swagger, MySQL

---
+++ src/main/java/ba/sum/fpmoz/rezervacijatermina/StudentskiSustavKnjigeApplication.java
@@ -0,0 +1,13 @@
+package ba.sum.fpmoz.rezervacijatermina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentskiSustavKnjigeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentskiSustavKnjigeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ba/sum/fpmoz/rezervacijatermina/StudentskiSustavKnjigeApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.sum.fpmoz.rezervacijatermina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentskiSustavKnjigeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
